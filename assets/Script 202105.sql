195c2a76a66d94fe6521708e94f734dec72b60dc
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-05-23 23:25:44 -0500

    Vectorización entre personajes ok.

--- assets/Script 202105.sql
+++ assets/Script 202105.sql
@@ -5,7 +5,7 @@ USE bd_cuenteros;
 show tables;
 
 
-#Una llave primaria puede ser llave for·nea.
+#Una llave primaria puede ser llave for√°nea.
 CREATE TABLE tb_proyectos
 (
 	id_proyecto		int 				not null auto_increment,
@@ -47,3 +47,38 @@ create table tb_capitulos_objetos
 	fecha_registro 		datetime			not null,
 	primary key( id_capitulo, id_objeto )
 );
+
+
+create table tb_tipo_vectorizacion
+(
+	id_tipo_vectorizacion 		int			not null 	auto_increment,
+	desc_vectorizacion		varchar(20)		not null,
+	fecha_registro 			datetime		not null,
+	primary key( id_tipo_vectorizacion )
+);
+
+
+create table tb_vectorizados
+(
+	id_vectorizacion		int		not null 	auto_increment,		
+	id_capitulo			int		NOT null,
+	id_objeto_vectoriza		varchar(100) 	not null,
+	id_objeto_vectorizado		varchar(100) 	not null,
+	id_tipo_vectorizacion 		int			not null,
+	fecha_registro		datetime			not null,
+	primary key ( id_vectorizacion )
+);
+
+
+alter table tb_vectorizados
+ADD CONSTRAINT fk_vectorizados_caps_objetos_1 
+FOREIGN KEY (id_objeto_vectoriza) 
+REFERENCES tb_capitulos_objetos (id_objeto) 
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+alter table tb_vectorizados
+ADD CONSTRAINT fk_vectorizados_caps_objetos_2 
+FOREIGN KEY (id_objeto_vectorizado) 
+REFERENCES tb_capitulos_objetos (id_objeto) 
+ON DELETE CASCADE ON UPDATE CASCADE;
+
