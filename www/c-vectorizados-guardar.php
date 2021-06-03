93b6d796906d9d017bcb35b9e9ceced5edbb5580
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-06-03 11:00:35 -0500

    Lista inserción de vectores

--- www/c-vectorizados-guardar.php
+++ www/c-vectorizados-guardar.php
@@ -7,15 +7,15 @@
     //$seccion = "v-capitulo-insertar.php";
     //include( "v-plantilla.php" );
 
-    echo $_GET[ 'lista_capitulos' ];
-    echo $_GET[ 'lista_objetos1' ];
-    echo $_GET[ 'lista_objetos2' ];
-    echo $_GET[ 'lista_vectorizados' ];
-    echo $_GET[ 'lista_tipo_vectores' ];
-    
+    $lista_capitulos = $_GET[ 'lista_capitulos' ];
+    $lista_objetos1 = $_GET[ 'lista_objetos1' ];
+    $lista_objetos2 = $_GET[ 'lista_objetos2' ];
+    $lista_vectorizados = $_GET[ 'lista_vectorizados' ];
+    $lista_tipo_vectores = $_GET[ 'lista_tipo_vectores' ];
+
     //echo $_GET[ 'lista_objetos' ];
 
-    /*$r = Inserciones::insertar_objetos_capitulos( $_GET[ 'lista_capitulos' ], $_GET[ 'lista_objetos' ] );
+    $r = Inserciones::insertar_vectorizaciones( $lista_capitulos, $lista_objetos1, $lista_objetos2, $lista_tipo_vectores, 0, $lista_vectorizados, '' );
     
     if( $r == 1 )
     {
@@ -23,8 +23,8 @@
 
     }else{
 
-        header( "location: c-objetos-capitulos.php" );
+        header( "location: c-vectorizados.php" );
 
-    }*/
+    }
 
     
