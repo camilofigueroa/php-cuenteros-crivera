8db5138d52f8974d29f69638a93a39a991d464ef
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-06-03 11:53:43 -0500

    Otros controles al insert de vectorizados

--- www/c-vectorizados-guardar.php
+++ www/c-vectorizados-guardar.php
@@ -12,10 +12,12 @@
     $lista_objetos2 = $_GET[ 'lista_objetos2' ];
     $lista_vectorizados = $_GET[ 'lista_vectorizados' ];
     $lista_tipo_vectores = $_GET[ 'lista_tipo_vectores' ];
+    $lista_estado = $_GET[ 'lista_estado' ];
+    $observaciones = $_GET[ 'observaciones' ];
 
     //echo $_GET[ 'lista_objetos' ];
 
-    $r = Inserciones::insertar_vectorizaciones( $lista_capitulos, $lista_objetos1, $lista_objetos2, $lista_tipo_vectores, 0, $lista_vectorizados, '' );
+    $r = Inserciones::insertar_vectorizaciones( $lista_capitulos, $lista_objetos1, $lista_objetos2, $lista_tipo_vectores, $lista_estado, $lista_vectorizados, $observaciones );
     
     if( $r == 1 )
     {
--- www/v-vectorizados.php
+++ www/v-vectorizados.php
@@ -1,18 +1,28 @@
 
 <form action="c-vectorizados-guardar.php">
   
-  Selecciona el capítulo en donde está tu objeto: <?= $lista_capitulos ?>
-  <br>
+    Selecciona el capítulo en donde está tu objeto: <?= $lista_capitulos ?>
+    <br>
 
-  <div id="salida"></div>
+    <div id="salida"></div>
 
-  Selecciona el tipo de vectorización: <?= $lista_tipo_vector ?>
-  <br>
+    Selecciona el tipo de vectorización: <?= $lista_tipo_vector ?>
+    <br>
 
-  Selecciona la vectorización que originó esta: <?= $lista_vectorizados ?>
-  <br>
+    Selecciona la vectorización que originó esta: <?= $lista_vectorizados ?>
+    <br>
 
-  <input type="submit" value="Guardar.">
+    <select name="lista_estado">
+        <option value="0">Vectorización abierta</option>
+        <option value="1">Ciere de vectorización</option>
+    </select>
+    
+    <br>
+
+    <input type="text" name="observaciones" required>
+    <br>
+
+    <input type="submit" value="Guardar.">
   
 </form>
 
