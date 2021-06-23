c3e44a05a0ee1e297ad0b4ab095a51ff5d73485b
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-06-22 22:40:42 -0500

    Sitio definitivo assets 20210622

--- assets/Script-generales.sql
+++ assets/Script-generales.sql
@@ -20,7 +20,7 @@ select max_proximo_orden_capitulo( 1, 3, 1 );
 drop function cambiar_orden_capitulo;
 select cambiar_orden_capitulo( 1, 7, 6 );
 
-#Permite traer lac combinatorias de capítulo vs objetos.
+#Permite traer las combinatorias de capítulo vs objetos.
 select id_objeto, titulo_capitulo,
 ( 	select count( * ) from tb_capitulos t11, tb_objetos t21, tb_capitulos_objetos tco 
 	where t1.id_capitulo = t11.id_capitulo 
@@ -38,17 +38,23 @@ order by id_objeto, titulo_capitulo;
 ALTER TABLE `tb_capitulos_objetos` ADD `muestra_texto` VARCHAR(3000) NULL AFTER `id_objeto`;
 
 
-select t1.id_capitulo, t2.id_objeto, t2.tipo_objeto, t2.fecha_registro, 
-t4.titulo_capitulo, t1.muestra_texto, 
-case 
-	when instr( t4.texto, t1.muestra_texto ) > 0 
-	then 'si está'
-	else 'no está'
-end as mirando
-from tb_capitulos_objetos t1, tb_objetos t2, tb_tipos_objeto t3, tb_capitulos t4 
-where t1.id_objeto = t2.id_objeto 
-and t2.tipo_objeto = t3.tipo_objeto 
-and t1.id_capitulo = t4.id_capitulo  
-order by t1.id_capitulo, t2.fecha_registro; 
+#Select del árbol de palabras.
+select CONCAT( 'cap ', t1.id_capitulo, '-', length( t4.texto ) ) as cap, 
+t1.id_objeto, CONCAT( '(', t2.id_vectorizacion, ')' ) as id_v, 
+t3.desc_vectorizacion, t2.id_objeto_vectorizado, 
+case when t2.id_vectorizacion = t2.id_vectorizacion_padre then '' else CONCAT( '(', t2.id_vectorizacion_padre, ')' ) end as id_v_p, orden 
+from tb_capitulos_objetos t1, tb_vectorizados t2, tb_tipo_vectorizacion t3, tb_capitulos t4 
+where t1.id_objeto = t2.id_objeto_vectoriza 
+and t1.id_capitulo = t2.id_capitulo 
+and t2.id_tipo_vectorizacion = t3.id_tipo_vectorizacion 
+and t1.id_capitulo = t4.id_capitulo 
+and t4.id_proyecto = 1
+union 
+select CONCAT( 'cap ', tc.id_capitulo, '-', length( tc.texto ) ) as cap, 0, '', '', '', '', orden 
+from tb_capitulos tc 
+where not exists ( select 1 from tb_capitulos_objetos tco where tc.id_capitulo = tco.id_capitulo ) 
+and tc.id_proyecto = 1 order by orden, cap ;
+
+
 
 
