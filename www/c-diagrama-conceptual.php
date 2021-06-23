dd352f95f6abf61264d93499dabaa224c5c95999
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2021-06-23 12:44:58 -0500

    Agregando objetos al d conceptual www

--- www/c-diagrama-conceptual.php
+++ www/c-diagrama-conceptual.php
@@ -6,7 +6,8 @@
 
     Sesiones::verificaciones_generales( [ 1, "id_proyecto", "c-proyecto-seleccionar.php" ] );
 
-    $r = Vimprimir::diagrama_conceptual( Consultas::consultar_dato( "tb_capitulos", "id_capitulo, titulo_capitulo", "orden limit 5", "id_proyecto = ".$_SESSION[ 'id_proyecto' ] ) );
+    //$r = Vimprimir::diagrama_conceptual( Consultas::consultar_dato( "tb_capitulos", "id_capitulo, titulo_capitulo", "orden", "id_proyecto = ".$_SESSION[ 'id_proyecto' ] ) );
+    $r = Vimprimir::diagrama_conceptual( Consultas::traer_capitulo_objetos( $_SESSION[ 'id_proyecto' ], null, 4 ) );
 
     //La variable cabecera se usará para incluir porciones javascript como en los gráficos de Google.
     $cabecera = "c-diagrama-conceptual-cabecera.php";
